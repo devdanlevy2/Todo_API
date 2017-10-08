b0f5be0da0e7edc8320ddf25bf0da1abc850bb01
devdanlevy <daniel.levy2@icloud.com>
2017-10-07 20:59:39 -0400

    added methods to TodosController

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,34 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  def index
+    @todos = Todo.all?
+    json_resoponse(@todos)
+  end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
+  end
+
+  def show
+    json_response(@todo)
+  end
+
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  def todo_params
+    @todo = Todo.find(params[:id])
+  end
+  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,14 +14,16 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-module RequestSpecHelper
-  # Parse JSON response to ruby hash
-  def json
-    JSON.parse(response.body)
-  end
-end
+
 
 RSpec.configure do |config|
+
+  module RequestSpecHelper
+    # Parse JSON response to ruby hash
+    def json
+      JSON.parse(response.body)
+    end
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
